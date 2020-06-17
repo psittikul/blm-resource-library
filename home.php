904b8341b948010521592b915616352c34a50542
Patricia Sittikul <pas217@lehigh.edu>
2020-06-17 18:46:12 -0400

    Installed WordPress on localhost

---
+++ home.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Home page template file
+ *
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage BLM_Resource_Library
+ */
+
+get_header();
+?>
+
+<main id="site-content" role="main">
+
+
+</main><!-- #site-content -->
+
+
+<?php
+get_footer();
