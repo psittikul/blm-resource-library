0bd0701c9c31aa78697b6a09b50bd245eb6cea7d
Patricia Sittikul <pas217@lehigh.edu>
2020-06-18 01:44:21 -0400

    Fixing sidenav thing on live site

--- template-parts/sidenav.php
+++ template-parts/sidenav.php
@@ -10,19 +10,19 @@ $allCategories = get_categories();
 
     <ul>
         <?php
-        $currCategory = array_filter($allCategories, function ($c) {
-            $curr = get_page(get_the_ID())->post_name;
-            return $c->slug == $curr;
-        });
-        $cid = $currCategory[0]->term_id;
-        echo "<h4>$cid</h4>";
+        // $currCategory = array_filter($allCategories, function ($c) {
+        //     $curr = get_page(get_the_ID())->post_name;
+        //     return $c->slug == $curr;
+        // });
+        // $cid = $currCategory[0]->term_id;
+        echo get_page(get_the_ID())->post_name;
         $subcategories = array();
-        foreach ($allCategories as $subcategory) {
-            if ($subcategory->parent == $cid) {
-                array_push($subcategories, $subcategory);
-            }
-        }
-        echo var_dump($subcategories);
+        // foreach ($allCategories as $subcategory) {
+        //     if ($subcategory->parent == $cid) {
+        //         array_push($subcategories, $subcategory);
+        //     }
+        // }
+        // echo var_dump($subcategories);
         ?>
         <!-- <a href="#<?php echo $subcategory->slug; ?>" data-supposed="<?php echo $cid; ?>" data-pid="<?php echo $subcategory->parent; ?>" data-cid="<?php echo $subcategory->term_id; ?>">
                     <li><?php echo $subcategory->name; ?></li>
