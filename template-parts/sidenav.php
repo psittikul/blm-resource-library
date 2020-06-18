48dea3ffa7b154ab617464408e03d33f8618850d
Patricia Sittikul <pas217@lehigh.edu>
2020-06-18 01:39:09 -0400

    Fixing sidenav thing on live site

--- template-parts/sidenav.php
+++ template-parts/sidenav.php
@@ -2,12 +2,7 @@
 
 /** TO-DO: Use wpquery or whatever to get all the subcategories of the current category page */
 $allCategories = get_categories();
-$currCategory = array_filter($allCategories, function ($c) {
-    $curr = get_page(get_the_ID())->post_name;
-    return $c->slug == $curr;
-});
 
-$cid = $currCategory[0]->term_id;
 
 ?>
 
@@ -15,9 +10,15 @@ $cid = $currCategory[0]->term_id;
 
     <ul>
         <?php
+        $currCategory = array_filter($allCategories, function ($c) {
+            $curr = get_page(get_the_ID())->post_name;
+            return $c->slug == $curr;
+        });
+
+        $topID = $currCategory[0]->term_id;
         foreach ($allCategories as $subcategory) {
             // Only choose the subcategories of this current category page
-            if ($subcategory->parent == 5) {
+            if ($subcategory->parent == $topID) {
         ?>
                 <a href="#<?php echo $subcategory->slug; ?>" data-supposed="<?php echo $cid; ?>" data-pid="<?php echo $subcategory->parent; ?>" data-cid="<?php echo $subcategory->term_id; ?>">
                     <li><?php echo $subcategory->name; ?></li>
