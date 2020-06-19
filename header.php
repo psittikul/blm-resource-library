f8e2cf416f40714718e725bbc0f12f7e00c5e64d
Patricia Sittikul <pas217@lehigh.edu>
2020-06-18 23:57:00 -0400

    Changed max height of cards

--- header.php
+++ header.php
@@ -29,7 +29,7 @@
 
 <body>
 	<!-- Top nav here -->
-		<h1 id="pageTitle"><a href="/" >RESOURCE LIBRARY - BLACK LIVES MATTER MOVEMENT</a></h1>
+		<h1 id="pageTitle"><a href="/" >RESOURCE LIBRARY: BLACK LIVES MATTER MOVEMENT</a></h1>
 	<nav class="navbar navbar-expand-lg navbar-dark" id="topNav">
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 			<span class="navbar-toggler-icon"></span>
