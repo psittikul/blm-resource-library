bf54ae57a04991471579b0f5b3052efecc1a6a9f
Patricia Sittikul <pas217@lehigh.edu>
2020-06-25 15:05:29 -0400

    Working on adding slideshow view thing

--- single-resources.php
+++ single-resources.php
@@ -26,31 +26,44 @@ get_header();
      * TO-DO: Offer a "view all slides"/grid view button thing
      */
     if (get_field("slide_image_1", $rid)) {
-        echo var_dump(get_field("slide_image_1", $rid));
-        $fieldname = "slide_image_" . 2;
-        echo var_dump(get_field($fieldname, $rid));
-    } else {
-        echo "No slide images apparently";
+    ?>
+        <div id="graphicCarousel" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <!-- TO-DO: Actually do these lol -->
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <?php
+                // Go through each of the slideshow images
+                for ($i = 1; $i < 11; $i++) {
+                    $fieldname = "slide_image_$i";
+                    if (get_field($fieldname, $rid)) {
+                        $img = get_field($fieldname, $rid);
+                ?>
+                        <div class='carousel-item active'>
+                            <img src="<?php echo $img->url; ?>" alt="<?php echo $img->alt; ?>" />
+                        </div>
+                <?php
+                    } else {
+                        break;
+                    }
+                }
+                ?>
+            </div>
+            <a class="carousel-control-prev" href="#graphicCarousel" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#graphicCarousel" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    <?php
     }
     ?>
-    <!-- <div id="graphicCarousel" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-
-                </div>
-                <a class="carousel-control-prev" href="#graphicCarousel" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#graphicCarousel" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
-            </div> -->
     <?php
     echo get_field("full_content", $rid);
     ?>
