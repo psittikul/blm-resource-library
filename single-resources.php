e3e2d5cd5acdac13bcad971fd6f1348a6c0864ad
Patricia Sittikul <pas217@lehigh.edu>
2020-06-25 05:27:52 -0400

    Trying to implement infographic view thing

--- single-resources.php
+++ single-resources.php
@@ -18,8 +18,8 @@ get_header();
     $resource = get_post($rid); ?>
     <h2 class="resource-post-title"><?php echo the_title(); ?></h2>
     <p class="author-source">Author/source: <?php echo get_field("author_source", $rid); ?></p>
-    <?
-    echo "testing testing";
+    <?php
+    echo get_post_type($rid);
     echo $resource->post_content;
     echo get_field("full_content", $rid);
     ?>
