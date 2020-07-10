98aaa7869581f592bd4039d188ab88b0de1ea126
Patricia Sittikul <pas217@lehigh.edu>
2020-07-10 17:26:20 -0400

    Changed header to just say 'Firebrand Resource Library'

--- header.php
+++ header.php
@@ -31,7 +31,7 @@
 
 <body>
 	<!-- Top nav here -->
-	<h1 id="pageTitle"><a href="/">FIREBRAND RESOURCE LIBRARY: BLACK LIVES MATTER MOVEMENT</a></h1>
+	<h1 id="pageTitle"><a href="/">FIREBRAND RESOURCE LIBRARY</a></h1>
 	<nav class="navbar navbar-expand-lg navbar-dark" id="topNav">
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 
